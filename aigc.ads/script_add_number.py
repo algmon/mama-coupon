cfc3a5cfdbf315df7579d98845ef1e39d08af817
Wei Jiang <wei@suanfamama.com>
2024-07-03 22:02:20 +0800

    ADD aigc ads showcase

---
+++ aigc.ads/script_add_number.py
@@ -0,0 +1,19 @@
+from PIL import Image, ImageDraw, ImageFont
+import random
+
+def add_number_to_image(image_file, text):
+    """
+    Adds a number to the right bottom of an image.
+
+    Args:
+        image_file: The path to the image file.
+        text: The text to add to the image.
+
+    Returns:
+        The modified image.
+    """
+    pass
+
+ifn = "suanfamama.logo.png"
+text = "认知计算广告位置"
+add_number_to_image(ifn, text)
