2a33a749f490217387926bd96ab1ff77be29c297
Wei Jiang <wei@suanfamama.com>
2024-07-13 21:30:00 +0800

    LINT Project

---
+++ algs/alg0/use_yolo_for_fashion_detection.py
@@ -0,0 +1,34 @@
+import json
+import os
+import torch
+
+# 1. Data
+base = "./data/exp1.in"
+
+# Get a list of all the files in the directory
+imgs = os.listdir(base)
+
+# Add the file path base to the list of images
+imgs = [os.path.join(base, img) for img in imgs]
+
+# Print the list of files
+print(imgs)
+
+# 2. Model
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+
+# Images
+# imgs = ['./data/sample.1.png']  # batch of images
+
+# Inference
+results = model(imgs)
+
+# Results
+results.print()
+results.save()  # or .show()
+
+for i in range(0,len(imgs)):
+    print("img #:", i)
+    results.xyxy[i]  # img1 predictions (tensor)
+    print(results.pandas().xyxy[i])  # img1 predictions (pandas)
+    print()
