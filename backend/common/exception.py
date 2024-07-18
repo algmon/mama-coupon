58acff824d4c2f29cafac0b37ed37a19747ad9f6
Wei Jiang <wei@suanfamama.com>
2024-07-18 15:50:34 +0800

    LINT project for sales team

--- backend/common/exception.py
+++
@@ -1,6 +0,0 @@
-from fastapi import HTTPException
-def exception(code: int, detail: str):
-    """
-    根据提供的 code 和 detail 抛出一个 HTTP 异常。
-    """
-    raise HTTPException(status_code=code, detail=detail)
--- frontend/src/api/aiChat.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-export function fetchAnswer(data) {
-  return request({
-    url: '/aiChat/answer',
-    method: 'post',
-    data
-  })
-}
